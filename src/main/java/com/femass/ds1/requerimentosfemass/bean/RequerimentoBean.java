69621f76bf5c0eb60bd24b1fb8b81f7a9ce52b49
gustavo <gustavo@10.10.0.67>
2018-11-06 19:45:11 -0200

    Inclusão da tela abrir requerimento com a combobox do tipo do requerimento sendo preenchida

--- src/main/java/com/femass/ds1/requerimentosfemass/bean/RequerimentoBean.java
+++ src/main/java/com/femass/ds1/requerimentosfemass/bean/RequerimentoBean.java
@@ -1,5 +1,7 @@
 package com.femass.ds1.requerimentosfemass.bean;
 
+import com.femass.ds1.requerimentosfemass.dao.RequerimentoDao;
+import com.femass.ds1.requerimentosfemass.dao.TipoRequerimentoDao;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,174 +19,195 @@ import com.femass.ds1.requerimentosfemass.model.TipoRequerimento;
 import com.femass.ds1.requerimentosfemass.filter.RequerimentoFilter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import javax.ejb.EJB;
 
 @ManagedBean
 @ViewScoped
 public class RequerimentoBean {
-	private Requerimento cadastro;
-	private List<Requerimento> lista;
-	private List<Requerimento> lipesq;
-	private int size;
-	private String acao;
-	private RequerimentoFilter filtro;
 
-	public Requerimento getCadastro() {
-		if (cadastro == null) {
-			cadastro = new Requerimento();
-		}
-		return cadastro;
-	}
+    private Requerimento cadastro;
+    private List<Requerimento> lista;
+    private List<Requerimento> lipesq;
+    private List<TipoRequerimento> listatipo;
+    private int size;
+    private String acao;
+    private RequerimentoFilter filtro;
+    
+    
+    
+    @EJB
+    private RequerimentoDao dao;
+    
+    @EJB
+    private TipoRequerimentoDao tipoDao;
+    
 
-	public void setCadastro(Requerimento cadastro) {
-		this.cadastro = cadastro;
-	}
+    /**
+     * Metodo de abertura
+     */
+    public void carregar() {
+        try {
+            // cria aluno
+            Aluno aluno = new Aluno();
+            aluno.setId(1L);
+            aluno.setNome("Jocimar Galante");
 
-	public List<Requerimento> getLista() {
-		return lista;
-	}
+            Aluno aluno2 = new Aluno();
+            aluno2.setId(2L);
+            aluno2.setNome("Alex Santos");
 
-	public void setLista(List<Requerimento> lista) {
-		this.lista = lista;
-	}
+            // cria tipo do requerimento
+            TipoRequerimento treq = new TipoRequerimento();
+            treq.setId(1L);
+            treq.setNome("Assinatura do Contrato de estágio");
+            treq.setSetor("Recepção");
 
-	public List<Requerimento> getLipesq() {
-		return lipesq;
-	}
+            TipoRequerimento treq2 = new TipoRequerimento();
+            treq2.setId(2L);
+            treq2.setNome("Dispensa de Disciplina");
+            treq2.setSetor("Coordenação");
 
-	public void setLipesq(List<Requerimento> lipesq) {
-		this.lipesq = lipesq;
-	}
+            lista = new ArrayList<>();
 
-	public int getSize() {
-		return size;
-	}
+            Requerimento req = new Requerimento();
+            req.setId(1L);
+            req.setNumeroProtocolo("0001/2018");
+            req.setDescricao("Entrega de contrato para avaliação e assinatura.");
+            req.setRevisao(false);
+            req.setStatusRequerimento(StatusRequerimento.Aberto);
+            req.setTipoRequerimento(treq);
+            req.setAluno(aluno);
+            req.setDataAbertura(new Date());
+            lista.add(req);
 
-	public void setSize(int size) {
-		this.size = size;
-	}
+            Requerimento req2 = new Requerimento();
+            req2.setId(2L);
+            req2.setNumeroProtocolo("0002/2018");
+            req2.setDescricao("Em anexo seguem as comprovações das matérias concluídas em outro curso técnico para avaliação.");
+            req2.setRevisao(false);
+            req2.setStatusRequerimento(StatusRequerimento.Aberto);
+            req2.setTipoRequerimento(treq2);
+            req2.setAluno(aluno2);
+            req2.setDataAbertura(new Date());
+            lista.add(req2);
 
-	public String getAcao() {
-		return acao;
-	}
+            size = lista.size();
+            System.out.println("Tamanho da lista = " + size);
 
-	public RequerimentoFilter getFiltro() {
-		if (filtro == null) {
-			filtro = new RequerimentoFilter();
-		}
-		return filtro;
-	}
+        } catch (RuntimeException e) {
+            System.out.println("Erro ao tentar gerar a lista.");
+        }
 
-	public void setFiltro(RequerimentoFilter filtro) {
-		this.filtro = filtro;
-	}
+    }
 
-	/**
-	 * Metodo de abertura
-	 */
-	public void carregar() {
-		try {
-			// cria aluno
-			Aluno aluno = new Aluno();
-			aluno.setId(1L);
-			aluno.setNome("Jocimar Galante");
+    public void consultar() {
+        try {
 
-			Aluno aluno2 = new Aluno();
-			aluno2.setId(2L);
-			aluno2.setNome("Alex Santos");
+            carregar();
 
-			// cria tipo do requerimento
-			TipoRequerimento treq = new TipoRequerimento();
-			treq.setId(1L);
-			treq.setNome("Assinatura do Contrato de estágio");
-			treq.setSetor("Recepção");
+            Requerimento req2 = null;
+            for (Requerimento req : lista) {
+                if (filtro.getProtocolo().equals(req.getNumeroProtocolo())) {
+                    lipesq = new ArrayList<>();
+                    lipesq.add(req);
+                    req2 = req;
+                }
+            }
 
-			TipoRequerimento treq2 = new TipoRequerimento();
-			treq2.setId(2L);
-			treq2.setNome("Dispensa de Disciplina");
-			treq2.setSetor("Coordenação");
+            if (req2 == null) {
+                Messages.addGlobalInfo("Protocolo Não encontrado.");
+                return;
+            }
+        } catch (RuntimeException e) {
+            Messages.addGlobalError("Erro ao tentar consultar um processo." + e.getMessage());
+        }
 
-			lista = new ArrayList<>();
+    }
 
-			Requerimento req = new Requerimento();
-			req.setId(1L);
-			req.setNumeroProtocolo("0001/2018");
-			req.setDescricao("Entrega de contrato para avaliação e assinatura.");
-			req.setRevisao(false);
-			req.setStatusRequerimento(StatusRequerimento.Aberto);
-			req.setTipoRequerimento(treq);
-			req.setAluno(aluno);
-			req.setDataAbertura(new Date());
-			lista.add(req);
+    public void revisar() {
+        if (cadastro != null) {
+            cadastro.setRevisao(true);
+        }
+    }
 
-			Requerimento req2 = new Requerimento();
-			req2.setId(2L);
-			req2.setNumeroProtocolo("0002/2018");
-			req2.setDescricao("Em anexo seguem as comprovações das matérias concluídas em outro curso técnico para avaliação.");
-			req2.setRevisao(false);
-			req2.setStatusRequerimento(StatusRequerimento.Aberto);
-			req2.setTipoRequerimento(treq2);
-			req2.setAluno(aluno2);
-			req2.setDataAbertura(new Date());
-			lista.add(req2);
+    /**
+     * Metodo de selecao do data table pela barra de botoes
+     *
+     * @param evento
+     */
+    public void onRowSelect(SelectEvent evento) {
+        cadastro = (Requerimento) evento.getObject();
+        acao = "Editar";
+    }
 
-			size = lista.size();
-			System.out.println("Tamanho da lista = " + size);
+    /**
+     * Metodo Bean novo - Cria um novo objeto.
+     */
+    public void novo() {
+        cadastro = new Requerimento();
+        listatipo = tipoDao.getTipoRequerimentos();
+        acao = "Salvar";
+        cadastro.setDataAbertura(new Date());
+        cadastro.setStatusRequerimento(StatusRequerimento.Aberto);
+    }
+    
+    
+    public Requerimento getCadastro() {
+        if (cadastro == null) {
+            cadastro = new Requerimento();
+        }
+        return cadastro;
+    }
 
-		} catch (RuntimeException e) {
-			System.out.println("Erro ao tentar gerar a lista.");
-		}
+    public void setCadastro(Requerimento cadastro) {
+        this.cadastro = cadastro;
+    }
 
-	}
+    public List<Requerimento> getLista() {
+        return lista;
+    }
 
-	public void consultar() {
-		try {
-	
-			carregar();
+    public void setLista(List<Requerimento> lista) {
+        this.lista = lista;
+    }
 
-			Requerimento req2 = null;
-			for (Requerimento req : lista) {
-				if (filtro.getProtocolo().equals(req.getNumeroProtocolo())) {
-					lipesq = new ArrayList<>();
-					lipesq.add(req);
-					req2 = req;
-				}
-			}
+    public List<Requerimento> getLipesq() {
+        return lipesq;
+    }
 
-			if (req2 == null) {
-				Messages.addGlobalInfo("Protocolo Não encontrado.");
-				return;
-			}
-		} catch (RuntimeException e) {
-			Messages.addGlobalError("Erro ao tentar consultar um processo." + e.getMessage());
-		}
+    public void setLipesq(List<Requerimento> lipesq) {
+        this.lipesq = lipesq;
+    }
 
-	}
-	
-	
-	public void revisar(){
-		if(cadastro != null){
-			cadastro.setRevisao(true);
-		}
-	}
-	
-	/**
-	 * Metodo de selecao do data table pela barra de botoes
-	 * 
-	 * @param evento
-	 */
-	public void onRowSelect(SelectEvent evento) {
-		cadastro = (Requerimento) evento.getObject();
-		acao = "Editar";
-	}
+    public int getSize() {
+        return size;
+    }
 
-	/**
-	 * Metodo Bean novo - Cria um novo objeto.
-	 */
-	public void novo() {
-		cadastro = new Requerimento();
-		acao = "Salvar";
-		cadastro.setDataAbertura(new Date());
-		cadastro.setStatusRequerimento(StatusRequerimento.Aberto);
-	}
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getAcao() {
+        return acao;
+    }
+
+    public RequerimentoFilter getFiltro() {
+        if (filtro == null) {
+            filtro = new RequerimentoFilter();
+        }
+        return filtro;
+    }
+
+    public void setFiltro(RequerimentoFilter filtro) {
+        this.filtro = filtro;
+    }
+
+    public List<TipoRequerimento> getListatipo() {
+        return listatipo;
+    }
+
+    public void setListatipo(List<TipoRequerimento> listatipo) {
+        this.listatipo = listatipo;
+    }
 
 }
